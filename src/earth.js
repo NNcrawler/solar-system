dd4deb8821e43fa2bdac5253d549690a28cb8a6b
Rasyid Hakim <muhammad.hakim@go-jek.com>
2019-02-24 11:52:05 +0530

    Add earth

--- src/earth.js
+++ src/earth.js
@@ -1,18 +1,18 @@
 import { Point, loader, Sprite } from 'pixi.js';
-import sunSvg from '../assets/sun.svg';
+import earthSvg from '../assets/earth.svg';
 
-export default class Sun {
+export default class Earth {
   constructor(screenWidth, screenHeight) {
-    this.position = new Point(screenWidth / 2, screenHeight / 2);
-    this.length = 100;
+    this.position = new Point(screenWidth / 3, screenHeight / 3);
+    this.length = 50;
     this.pSpriteObj;
   }
 
   load(stage) {
     return new Promise((resolve, reject) => {
       try {
-        loader.add(sunSvg).load(() => {
-          this.pSpriteObj = new Sprite(loader.resources[sunSvg].texture);
+        loader.add(earthSvg).load(() => {
+          this.pSpriteObj = new Sprite(loader.resources[earthSvg].texture);
           this.pSpriteObj.position = this.position;
           this.pSpriteObj.width = this.pSpriteObj.height = this.length;
           this.pSpriteObj.anchor.set(0.5, 0.5);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as P from 'pixi.js';
 import "@babel/polyfill";
 import Sun from './sun.js';
+import Earth from './earth.js';
 
 document.addEventListener("DOMContentLoaded", async function(event) 
 { 
@@ -8,7 +9,10 @@ document.addEventListener("DOMContentLoaded", async function(event)
   app.renderer.backgroundColor = 0x282c34
   document.body.appendChild(app.view);
   const sun = new Sun(window.outerWidth, window.outerHeight);
+  const earth = new Earth(window.outerWidth, window.outerHeight);
   await sun.load(app.stage);
+  await earth.load(app.stage);
   sun.rotate(app.ticker, 0.01);
+  earth.rotate(app.ticker, 0.005);
 });
 
