32c0acb20a199fff6932464b1e0b312061b374b7
Rasyid Hakim <muhammad.hakim@go-jek.com>
2019-02-24 16:55:08 +0530

    Add star class

---
+++ src/meteor.js
@@ -0,0 +1,28 @@
+import { Sprite, loader, Point } from 'pixi.js';
+import starPng from '../assets/star.png';
+
+export default class Meteor {
+  constructor(x, y, length) {
+    this.options = {
+      position: new Point(x, y),
+      length
+    };
+  }
+
+  load(stage) {
+    return new Promise((resolve, reject) => {
+      try {
+        loader.add(starPng).load(() => {
+          this.pSpriteObj = new Sprite(loader.resources[starPng].texture);
+          this.pSpriteObj.position = this.options.position;
+          this.pSpriteObj.width = this.pSpriteObj.height = this.options.length;
+          stage.addChild(this.pSpriteObj);
+          console.log(this.pSpriteObj)
+          resolve();
+        });
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+}
